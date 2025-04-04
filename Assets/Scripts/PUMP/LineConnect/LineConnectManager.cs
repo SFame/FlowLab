12653f7f5b7527ade586563ae7c2ff0bc01eb8c1
SFame <star980823@naver.com>
2025-04-04 22:36:23 +0900

    Update LineConnectManager.cs

--- Assets/Scripts/PUMP/LineConnect/LineConnectManager.cs
+++ Assets/Scripts/PUMP/LineConnect/LineConnectManager.cs
@@ -18,8 +18,8 @@ public class LineConnectManager : MonoBehaviour
         lineGo.transform.SetParent(transform);
         
         LineConnector lc = lineGo.AddComponent<LineConnector>();
-        lc.LineRenderer.SetColor(Color.red);
-        lc.LineUpdated += lc.LineRenderer.SetAllDirty;
+        //lc.LineRenderer.SetColor(Color.red);
+        //lc.LineUpdated += lc.LineRenderer.SetAllDirty;
         lc.OnDragEnd += ((IChangeObserver)m_Background).ReportChanges;
         _lineRefreshAction += lc.RefreshPoints;
         lc.OnRemove += () => _lineRefreshAction -= lc.RefreshPoints;
