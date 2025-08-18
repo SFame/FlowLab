74670fcff4ab186037038be579aa4b3e15731654
mangom0 <michael0516@naver.com>
2025-08-18 22:11:45 +0900

    clockgate

---
+++ Assets/Scripts/PUMP/Node/Detail/Clockgate.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clockgate : Node
+{
+    protected override string NodeDisplayName => throw new System.NotImplementedException();
+
+    protected override List<string> InputNames => throw new System.NotImplementedException();
+
+    protected override List<string> OutputNames => throw new System.NotImplementedException();
+
+    protected override List<TransitionType> InputTypes => throw new System.NotImplementedException();
+
+    protected override List<TransitionType> OutputTypes => throw new System.NotImplementedException();
+
+    protected override float InEnumeratorXPos => throw new System.NotImplementedException();
+
+    protected override float OutEnumeratorXPos => throw new System.NotImplementedException();
+
+    protected override float EnumeratorSpacing => throw new System.NotImplementedException();
+
+    protected override Vector2 DefaultNodeSize => throw new System.NotImplementedException();
+
+    protected override Transition[] SetOutputInitStates(int outputCount, TransitionType[] outputTypes)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void StateUpdate(TransitionEventArgs args)
+    {
+        throw new System.NotImplementedException();
+    }
+}
